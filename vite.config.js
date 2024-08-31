937193d301f3d608dcf9a056f2b07f1c075e3892
Arthur de Paula Correa <arthur.correa@DSS.LOCAL>
2024-08-31 00:53:38 -0300

    Installation: Automatic Component Configuration Vue.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      dirs: ["src/components"], // Pasta onde os componentes estão localizados
+      extensions: ["vue"], // Extensões a serem consideradas como componentes
+      deep: true, // Habilita a busca recursiva em subpastas
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
