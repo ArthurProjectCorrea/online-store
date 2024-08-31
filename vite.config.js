e5659532b373593707389a310f550b562bd24fab
Arthur de Paula Correa <arthur.correa@DSS.LOCAL>
2024-08-31 01:07:27 -0300

    Installation: gh-pages configuration

--- vite.config.js
+++ vite.config.js
@@ -19,5 +19,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === "production" ? "/nome-do-repositorio/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/online-store/" : "/",
 });
